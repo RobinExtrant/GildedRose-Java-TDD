afd2ffe7bd17ce45a217efbd560472f82f8d5d0e
extrant.robin <robin.extrant@iut-valence.fr>
2015-11-03 08:38:51 +0100

    creation of test : updateItemsEachDay and modification of GildedRose updateItem method to make test success

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -2,14 +2,28 @@ package fr.iutvalence.info.dut.m3105.gildedroseinn.refactoring;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.Test;
 public class GildedRoseTest
 {
-	@Test
+	/*@Test
 	public void aSimpleBeginningTest()
 	{
 		assertEquals(true,false);
 	}
-
+*/
+	@Test
+	public void updateItemsEachDay()
+	{
+		
+		Item item = new Item("+5 Dexterity Vest", 10, 20);
+		for (int numDay=0;numDay<=1;numDay++)
+			{
+				GildedRose.updateItem(item);
+			}
+		assertEquals(item.getQuality(),18);
+		assertEquals(item.getSellIn(),8);
+	}
 }
